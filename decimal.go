d77807c8ffaf7c289f4863e6f96acf11f1509a08
sdcoffey <stevendcoffey@gmail.com>
2021-02-26 09:24:09 -0800

    NaN support

--- decimal.go
+++ decimal.go
@@ -5,12 +5,14 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"math"
 	"math/big"
 )
 
 var (
 	flZero = *big.NewFloat(0)
 
+	NaN  = NewDecimal(math.NaN())
 	ZERO = NewDecimal(0)
 	ONE  = NewDecimal(1)
 	TEN  = NewDecimal(10)
@@ -24,15 +26,34 @@ type Decimal struct {
 }
 
 // NewDecimal creates a new Decimal type from a float value.
-func NewDecimal(fl float64) Decimal {
-	return Decimal{big.NewFloat(fl)}
+func NewDecimal(val float64) Decimal {
+	var fl *big.Float
+
+	defer func() {
+		if r := recover(); r != nil {
+			switch r.(type) {
+			case big.ErrNaN:
+				fl = nil
+			}
+		}
+	}()
+
+	fl = big.NewFloat(val)
+
+	return Decimal{
+		fl: fl,
+	}
 }
 
 // NewFromString creates a new Decimal type from a string value.
 func NewFromString(str string) Decimal {
-	bfl := new(big.Float)
-	bfl.UnmarshalText([]byte(str))
-	return Decimal{bfl}
+	bfl := big.NewFloat(0)
+
+	if bfl, _, err := bfl.Parse(str, 10); err != nil {
+		return NaN
+	} else {
+		return Decimal{bfl}
+	}
 }
 
 // NewFromInt creates a new Decimal type from an int value
@@ -42,6 +63,12 @@ func NewFromInt(dec int) Decimal {
 
 // MaxSlice returns the max of a slice of decimals
 func MaxSlice(decimals ...Decimal) Decimal {
+	if anyNan(decimals...) {
+		return NaN
+	} else if len(decimals) == 0 {
+		return ZERO
+	}
+
 	initial := NewFromString("-Inf")
 
 	for _, decimal := range decimals {
@@ -55,6 +82,12 @@ func MaxSlice(decimals ...Decimal) Decimal {
 
 // MinSlice returns the min of a slice of decimals
 func MinSlice(decimals ...Decimal) Decimal {
+	if anyNan(decimals...) {
+		return NaN
+	} else if len(decimals) == 0 {
+		return ZERO
+	}
+
 	initial := NewFromString("Inf")
 	for _, decimal := range decimals {
 		if decimal.LT(initial) {
@@ -67,33 +100,47 @@ func MinSlice(decimals ...Decimal) Decimal {
 
 // Add adds a decimal instance to another Decimal instance.
 func (d Decimal) Add(addend Decimal) Decimal {
-	return Decimal{d.cpy().Add(d.fl, addend.fl)}
+	return nanGuard(func() Decimal {
+		return Decimal{d.cpy().Add(d.fl, addend.fl)}
+	}, d, addend)
 }
 
 // Sub subtracts another decimal instance from this Decimal instance.
 func (d Decimal) Sub(subtrahend Decimal) Decimal {
-	return Decimal{d.cpy().Sub(d.fl, subtrahend.fl)}
+	return nanGuard(func() Decimal {
+		return Decimal{d.cpy().Sub(d.fl, subtrahend.fl)}
+	}, d, subtrahend)
 }
 
 // Mul multiplies another decimal instance with this Decimal instance.
 func (d Decimal) Mul(factor Decimal) Decimal {
-	return Decimal{d.cpy().Mul(d.fl, factor.fl)}
+	return nanGuard(func() Decimal {
+		return Decimal{d.cpy().Mul(d.fl, factor.fl)}
+	}, d, factor)
 }
 
 // Div divides this Decimal by the denominator passed.
 func (d Decimal) Div(denominator Decimal) Decimal {
-	return Decimal{d.cpy().Quo(d.fl, denominator.fl)}
+	return nanGuard(func() Decimal {
+		return Decimal{d.cpy().Quo(d.fl, denominator.fl)}
+	}, d, denominator)
 }
 
 // Frac returns another Decimal instance representing this Decimal multiplied by the
 // provided float.
 func (d Decimal) Frac(f float64) Decimal {
-	return d.Mul(NewDecimal(f))
+	fractionFactor := NewDecimal(f)
+
+	return nanGuard(func() Decimal {
+		return d.Mul(NewDecimal(f))
+	}, d, fractionFactor)
 }
 
 // Neg returns this Decimal multiplied by -1.
 func (d Decimal) Neg() Decimal {
-	return d.Mul(NewDecimal(-1))
+	return nanGuard(func() Decimal {
+		return d.Mul(NewDecimal(-1))
+	}, d)
 }
 
 // Abs returns the absolute value of this Decimal
@@ -107,73 +154,118 @@ func (d Decimal) Abs() Decimal {
 
 // Pow returns the decimal to the inputted power
 func (d Decimal) Pow(exp int) Decimal {
-	if exp == 0 {
-		return ONE
-	}
+	return nanGuard(func() Decimal {
+		if exp == 0 {
+			return ONE
+		}
 
-	x := Decimal{d.cpy()}
+		x := Decimal{d.cpy()}
 
-	for i := 1; i < exp; i++ {
-		x = x.Mul(d)
-	}
+		for i := 1; i < exp; i++ {
+			x = x.Mul(d)
+		}
 
-	return x
+		return x
+	}, d)
 }
 
 // Sqrt returns the decimal's square root
 func (d Decimal) Sqrt() Decimal {
-	return Decimal{d.cpy().Sqrt(d.cpy())}
+	return nanGuard(func() Decimal {
+		return Decimal{d.cpy().Sqrt(d.cpy())}
+	}, d)
 }
 
 // EQ returns true if this Decimal exactly equals the provided decimal.
 func (d Decimal) EQ(other Decimal) bool {
+	if anyNan(d, other) {
+		return false
+	}
+
 	return d.Cmp(other) == 0
 }
 
 // LT returns true if this decimal is less than the provided decimal.
 func (d Decimal) LT(other Decimal) bool {
+	if anyNan(d, other) {
+		return false
+	}
+
 	return d.Cmp(other) < 0
 }
 
 // LTE returns true if this decimal is less or equal to the provided decimal.
 func (d Decimal) LTE(other Decimal) bool {
+	if anyNan(d, other) {
+		return false
+	}
+
 	return d.Cmp(other) <= 0
 }
 
 // GT returns true if this decimal is greater than the provided decimal.
 func (d Decimal) GT(other Decimal) bool {
+	if anyNan(d, other) {
+		return false
+	}
+
 	return d.Cmp(other) > 0
 }
 
 // GTE returns true if this decimal is greater than or equal to the provided decimal.
 func (d Decimal) GTE(other Decimal) bool {
+	if anyNan(d, other) {
+		return false
+	}
+
 	return d.Cmp(other) >= 0
 }
 
 // Cmp will return 1 if this decimal is greater than the provided, 0 if they are the same, and -1 if it is less.
 func (d Decimal) Cmp(other Decimal) int {
+	if anyNan(d, other) {
+		return 0
+	}
+
 	return d.fl.Cmp(other.fl)
 }
 
 // Float will return this Decimal as a float value.
 // Note that there may be some loss of precision in this operation.
 func (d Decimal) Float() float64 {
+	if d.NaN() {
+		return math.NaN()
+	}
+
 	f, _ := d.fl.Float64()
 	return f
 }
 
 // Zero will return true if this Decimal is equal to 0.
-// Note: Zero is deprecated. Use IsZero instead
+// Deprecated: Use IsZero instead
 func (d Decimal) Zero() bool {
 	return d.IsZero()
 }
 
+// Returns true if the underlying is not a valid number
+func (d Decimal) NaN() bool {
+	return d.fl == nil
+}
+
 // IsZero will return true if this Decimal is equal to 0.
 func (d Decimal) IsZero() bool {
+	if d.NaN() {
+		return false
+	}
+
 	return d.fl == nil || d.fl.Cmp(&flZero) == 0
 }
 
 func (d Decimal) String() string {
+	if d.NaN() {
+		return "NaN"
+	}
+
 	if d.fl == nil {
 		d.fl = new(big.Float)
 	}
@@ -182,6 +274,10 @@ func (d Decimal) String() string {
 }
 
 func (d Decimal) FormattedString(places int) string {
+	if d.NaN() {
+		return d.String()
+	}
+
 	format := "%." + fmt.Sprint(places) + "f"
 	fl := d.Float()
 	return fmt.Sprintf(format, fl)
@@ -234,4 +330,32 @@ func (d *Decimal) Scan(src interface{}) error {
 func (d Decimal) cpy() *big.Float {
 	cpy := new(big.Float)
 	return cpy.Copy(d.fl)
+}
+
+func anyNan(decimals ...Decimal) bool {
+	for _, decimal := range decimals {
+		if decimal.NaN() {
+			return true
+		}
+	}
+
+	return false
+}
+
+func allNan(decimals ...Decimal) bool {
+	for _, decimal := range decimals {
+		if !decimal.NaN() {
+			return false
+		}
+	}
+
+	return true
+}
+
+func nanGuard(yeildFunc func() Decimal, decimals ...Decimal) Decimal {
+	if anyNan(decimals...) {
+		return NaN
+	}
+
+	return yeildFunc()
 }
