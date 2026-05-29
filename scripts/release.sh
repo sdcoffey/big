7ccfddc607f554c20003304221f36aad37e383b3
sdcoffey <stevendcoffey@gmail.com>
2026-05-29 09:14:13 -0700

    Harden release tag detection

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,28 +3,28 @@
 set -euf -o pipefail
 
 echo -n "Version: "
-read newversion
+read -r newversion
 
-tag_count=`git tag --list | grep "$newversion" | wc -l | tr -d '[:space:]' || true`
+tag_name="v$newversion"
 
-if [[ $tag_count -gt 0 ]]; then
-  echo "Tag: $newversion already exists"
+if git rev-parse -q --verify "refs/tags/$tag_name" >/dev/null; then
+  echo "Tag: $tag_name already exists"
   exit 1
 else
   echo "Releasing $newversion"
 fi
 
 echo "Update CHANGELOG.md and press enter"
-read
+read -r
 
 git add CHANGELOG.md
 
-added_count=`git status --porcelain | grep "CHANGELOG.md" | wc -l | tr -d '[:space:]' || true`
+added_count=$(git status --porcelain | grep "CHANGELOG.md" | wc -l | tr -d '[:space:]' || true)
 if [[ $added_count -gt 0 ]]; then
   git commit -m"Release version $newversion"
 fi
 
-git tag "v$newversion" -m "v$newversion"
+git tag "$tag_name" -m "$tag_name"
 
 git push origin main
 git push --tags
